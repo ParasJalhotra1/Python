b354b3c065b59353f6bac8f968ba37a7efd2e399
ParasJalhotra1 <jalhotraparas@gmail.com>
2020-12-07 18:51:13 +0530

    added files

--- Day 2/MyPackage/modulePrime.py
+++ Day 2/MyPackage/modulePrime.py
@@ -5,13 +5,13 @@ def getPrime():
     __name__
 my_primes = []
 number= int(input("Enter Number To Prime Number: "))
-for pr in range(number, number+100):
+for num in range(number, number+100):
     isPrime = True
-    for i in range(2, pr):
-          if pr % i == 0:
+    for i in range(2, num):
+          if num % i == 0:
               isPrime = False
     if isPrime:
-        my_primes.append(pr)
+        my_primes.append(num)
 print(my_primes)
 
 if __name__ == '__main__':
