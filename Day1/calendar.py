034541287bc30864ba63b7adc5358830d0621779
ParasJalhotra1 <jalhotraparas@gmail.com>
2020-12-07 18:53:04 +0530

    added files

--- Day1/calendar.py
+++
@@ -1,5 +0,0 @@
-import calendar
-
-Year = int(input('Enter a year:'))
-month = int(input('Enter Month:'))
-print(calendar.month(Year,month))
--- Day1/prime.py
+++
@@ -1,8 +0,0 @@
-start = int(input('Enter Number:'))
-for num in range(start,start+100):
-  if num>1:
-    for i in range(2,num):
-        if(num % i==0):
-            break
-    else:
-        print(num)
